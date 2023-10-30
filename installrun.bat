2360abeae0768b1b865b06672c8638b1261f927e
isolationisbest <itwjnew@gmail.com>
2023-10-30 15:51:32 +0100

    cock ass wifi ass update whynot
    dsds

--- installrun.bat
+++ installrun.bat
@@ -1,7 +1,5 @@
 @echo off
 pip install -r requirements.txt
 cls
-echo press enter to continue
-pause >null
 py main.py
-pause >null
+pause >nul
--- main.py
+++ main.py
@@ -2,39 +2,36 @@ import aiohttp
 import asyncio
 import random
 import string
-from termcolor import colored
-
-# Number of codes to generate and check.
-num_codes = int(input("Enter the number of codes to generate and check: "))
+import colorama
+import sys
 
 valid_codes = []
-invalid_codes = []
+codes_per_second = 25  # Generate 100 codes per second
 
 async def check_code(session, code):
     async with session.get(f'https://discord.com/api/v10/entitlements/gift-codes/{code}') as response:
         if response.status == 200:
             valid_codes.append(code)
-        elif response.status == 404:
-            invalid_codes.append(code)
+            with open('valid.txt', 'a') as valid_file:
+                valid_file.write(f"https://discord.com/gifts/{code}\n")
+            print(f"{colorama.Fore.LIGHTGREEN_EX}VALID{colorama.Fore.RESET} | https://discord.com/gifts/{code}")
+            return True  # A valid code is found
+        else:
+            print(f"{colorama.Fore.LIGHTRED_EX}INVALID{colorama.Fore.RESET} | https://discord.com/gifts/{code}")
 
-async def main():
+async def generate_and_check():
     async with aiohttp.ClientSession() as session:
-        tasks = [check_code(session, ''.join(random.choices(string.ascii_uppercase + string.digits, k=16)) ) for _ in range(num_codes)]
-        await asyncio.gather(*tasks)
+        code_found = False
+        while not code_found:
+            tasks = []
+            for _ in range(codes_per_second):
+                code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
+                task = check_code(session, code)
+                tasks.append(task)
+
+            results = await asyncio.gather(*tasks)
+            code_found = any(results)
 
 if __name__ == '__main__':
-    asyncio.run(main())
-
-print("\n\033[1m\033[4mtitty gen\033[0m | Invalid: \033[91m{}\033[0m codes generated | Found: \033[92m{}\033[0m codes generated".format(len(invalid_codes), len(valid_codes)))
-
-print("\nValid Nitro Codes:")
-for code in valid_codes:
-    code_url = f"https://discord.com/gifts/{code}"
-    colored_code_url = colored(code_url, 'green')
-    print(colored("VALID |", 'green'), colored_code_url)
-
-print("\nInvalid Nitro Codes:")
-for code in invalid_codes:
-    code_url = f"https://discord.com/gifts/{code}"
-    colored_code_url = colored(code_url, 'red')
-    print(colored("INVALID |", 'red'), colored_code_url)
+    colorama.init(autoreset=True)  # Initialize colorama for colored output
+    asyncio.run(generate_and_check())
